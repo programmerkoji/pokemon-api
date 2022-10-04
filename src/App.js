20222a4ef5dc91e071e0605a6d396e071b81347c
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2022-10-04 21:45:29 +0900

    ページネーションの作成

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
 	const initialURL = "https://pokeapi.co/api/v2/pokemon";
 	const [loading, setLoading] = useState(true);
 	const [pokemonData, setPokemonData] = useState([]);
+	const [nextURL, setNextURL] = useState("");
+	const [prevURL, setPrevURL] = useState("");
 
 	useEffect(() => {
 		const fetchPokemonData = async () => {
@@ -17,7 +19,9 @@ function App() {
 
 			// 各ポケモンの詳細なデータ取得
 			loadPokemon(res.results);
-
+			// console.log(res.next);
+			setNextURL(res.next);
+			setPrevURL(res.previous);
 			setLoading(false);
 		};
 		fetchPokemonData();
@@ -31,10 +35,31 @@ function App() {
 				return pokemonRecord;
 			})
 		);
-		setPokemonData(_pokemonData);
+		setPokemonData(_pokemonData); //useStateのpokemonDataに格納
 	};
 
-	console.log(pokemonData);
+	// console.log(pokemonData);
+
+	const handleNextPage = async () => {
+		setLoading(true);
+		let data = await getAllPokemon(nextURL);
+		// console.log(data);
+		await loadPokemon(data.results);
+		setNextURL(data.next);
+		setPrevURL(data.previous);
+		setLoading(false);
+	};
+	const handlePrevPage = async () => {
+		if (!prevURL) return;
+
+		setLoading(true);
+		let data = await getAllPokemon(prevURL);
+		// console.log(data);
+		await loadPokemon(data.results);
+		setNextURL(data.next);
+		setPrevURL(data.previous);
+		setLoading(false);
+	};
 
 	return (
 		<>
@@ -48,6 +73,10 @@ function App() {
 							{pokemonData.map((pokemon, i) => {
 								return <Card key={i} pokemon={pokemon} />;
 							})}
+						</div>
+						<div className="btn">
+							<button onClick={handlePrevPage}>前へ</button>
+							<button onClick={handleNextPage}>次へ</button>
 						</div>
 					</>
 				)}
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -10,9 +10,9 @@ const Card = ({ pokemon }) => {
 			<h3 className="cardName">{pokemon.name}</h3>
 			<div className="cardTypes">
 				<div>タイプ</div>
-				{pokemon.types.map((type, i) => {
+				{pokemon.types.map((type) => {
 					return (
-						<div key={i}>
+						<div key={type.type.name}>
 							<span className="typeName">{type.type.name}</span>
 						</div>
 					);
